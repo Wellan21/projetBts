4f7253695849f799ee7f21c601df5a8b34884b74
Wellan21 <tristan.marcoux21@gmail.com>
2024-05-24 14:39:17 +0200

    test complilation constructeur

--- cmodelisteur.cpp
+++ cmodelisteur.cpp
@@ -1,17 +1,14 @@
 // cmodelisateur.cpp
 // Marcoux Tristan Avril 2024
-
 #include "cmodelisteur.h"
 #include <wiringPi.h>
 #include <string>
 #include <sstream>
 #include <cstdlib>
-#include <cmath>
-#include <iostream>
-#include <sqlite3.h>
+#include <math.h>
 
 // Constructeur
-Cmodelisateur::Cmodelisateur(int pinCapteur, int pinSteep, int pinDir, const char *pathDB, int nbPasMaxRasp, int nbPasMaxEsp, const char *ip, int port)
+Cmodelisateur::Cmodelisateur(int pinCapteur, int pinSteep, int pinDir, const char *pathDB, int nbPasMaxRasp, int nbPasMaxEsp, const char ip, int port)
 {
     // Initialisation des attributs de la classe
     this->pinCapteur = pinCapteur;
@@ -20,154 +17,191 @@ Cmodelisateur::Cmodelisateur(int pinCapteur, int pinSteep, int pinDir, const cha
     this->pathDB = pathDB;
     this->nbPasMaxEsp = nbPasMaxEsp;
     this->nbPasMaxRasp = nbPasMaxRasp;
-    this->client = clientSocket(ip, port);
-
-    // Configuration des broches
-    pinMode(this->pinSteep, OUTPUT);
+    this->client = clientSocket(ip, port)
+        // Configuration des broches
+        pinMode(this->pinSteep, OUTPUT);
     pinMode(this->pinDirection, OUTPUT);
     pinMode(this->pinCapteur, INPUT);
 }
 
-// Méthode pour écrire dans la base de données SQLite
-int Cmodelisateur::ecrireDB(const char *requete)
-{
-    sqlite3 *db;
-    char *errMsg = nullptr;
+// // Méthode pour écrire dans la base de données SQLite
+// int Cmodelisateur::ecrireDB(const char *requete)
+// {
+//     sqlite3 *db;
+//     char *errMsg = 0;
 
-    // Ouverture de la base de données
-    int rc = sqlite3_open("BDD_demonstrateur.db", &db);
+//     // Ouverture de la base de données
+//     int rc = sqlite3_open("BDD_demonstrateur.db", &db);
 
-    if (rc)
-    {
-        std::cerr << "Impossible d'ouvrir la base de données : " << sqlite3_errmsg(db) << std::endl;
-        return 0;
-    }
+//     if (rc)
+//     {
+//         std::cerr << "Impossible d'ouvrir la base de données : " << sqlite3_errmsg(db) << std::endl;
+//         return 0;
+//     }
+//     else
+//     {
+//         std::cout << "Base de données ouverte avec succès" << std::endl;
+//     }
 
-    // Exécution de la requête SQL
-    rc = sqlite3_exec(db, requete, nullptr, nullptr, &errMsg);
+//     // Requête SQL pour créer une table
+//     char createTableSQL[] = "INSERT INTO Campagne__de_photo (date, Chemin_d_acces,id_Piece) VALUES ('03/04/2024','/test',9)";
 
-    if (rc != SQLITE_OK)
-    {
-        std::cerr << "Erreur SQL : " << errMsg << std::endl;
-        sqlite3_free(errMsg);
-        sqlite3_close(db);
-        return 1;
-    }
+//     // Exécution de la requête SQL
+//     rc = sqlite3_exec(db, createTableSQL, 0, 0, &errMsg);
 
-    sqlite3_close(db);
-    return 0;
-}
+//     if (rc != SQLITE_OK)
+//     {
+//         std::cerr << "Erreur SQL : " << errMsg << std::endl;
+//         sqlite3_free(errMsg);
+//         // Fermeture de la base de données
+//         sqlite3_close(db);
+//         return 1;
+//     }
+//     else
+//     {
+//         std::cout << "Table créée avec succès" << std::endl;
+//         // Fermeture de la base de données
+//         sqlite3_close(db);
+//         return 0;
+//     }
+// }
 
-// Méthode pour déplacer le moteur pas à pas
-void Cmodelisateur::deplacerMot(int nbpas, int dir)
-{
-    wiringPiSetup();
+// // Méthode pour déplacer le moteur pas à pas
+// void Cmodelisateur::deplacerMot(int nbpas, int dir)
+// {
+//     wiringPisetup();
 
-    pinMode(this->pinSteep, OUTPUT);
-    pinMode(this->pinDirection, OUTPUT);
-    digitalWrite(this->pinDirection, dir);
-    for (int i = 0; i < nbpas; i++)
-    {
-        digitalWrite(this->pinSteep, HIGH); // Activé
-        delay(5);
-        digitalWrite(this->pinSteep, LOW);  // Désactivé
-        delay(5);
-    }
-}
+//     pinMode(this->pinSteep, OUTPUT);
+//     pinMode(DIR, OUTPUT);
+//     digitalWrite(this->pinDirection, dir);
+//     for (int i = 0; i < nbpas; i++)
+//     {
+//         digitalWrite(this->pinSteep, HIGH); // Activé
+//         delay(5);                           //
+//         digitalWrite(this->pinSteep, LOW);  // Désactivé
+//         delay(5);
+//     }
+//     return;
+// }
 
-// Méthode pour initialiser le système
-int Cmodelisateur::initSys()
-{
-    while (digitalRead(this->pinCapteur))
-    {
-        digitalWrite(this->pinSteep, HIGH); // Activé
-        delay(5);
-        digitalWrite(this->pinSteep, LOW);  // Désactivé
-        delay(5);
-    }
-    return 0;
-}
+// // Méthode pour initialiser le système
+// int Cmodelisateur::initSys()
+// {
+//     while (digitalRead(this->pinCapteur))
+//     {
+//         digitalWrite(this->pinSteep, HIGH); // Activé
+//         delay(5);                           //
+//         digitalWrite(this->pinSteep, LOW);  // Désactivé
+//         delay(5);
+//     }
+// }
 
-// Méthode pour envoyer une requête via un socket
-int Cmodelisateur::envoiRequeteSocket(const char *requete)
-{
-    return client.envoiMess(requete);
-}
+// // Méthode pour envoyer une requête via un socket
+// int Cmodelisateur::envoiRequeteSocket(const char *requete)
+// {
+//     return client.envoiMess(requete);
+// }
+// int Cmodelisateur : prendrePhoto(std::string *nomphoto)
+// {
+//     string commande = "ffmpeg -f v4l2 -video_size 1920x1080 -i /dev/video0 -frames 1 test/test" + nomPhoto + ".jpg";
+//     return system(command.c_str());
+// }
 
-// Méthode pour prendre une photo
-int Cmodelisateur::prendrePhoto(const std::string &nomphoto)
-{
-    std::string commande = "ffmpeg -f v4l2 -video_size 1920x1080 -i /dev/video0 -frames 1 " + nomphoto + ".jpg";
-    return system(commande.c_str());
-}
+// // Méthode pour initialiser le système et envoyer une requête
+// int Cmodelisateur::init()
+// {
+//     if (initSys() && envoiRequeteSocket("i"))
+//     {
+//         return 1;
+//     }
+//     else
+//     {
+//         return 0;
+//     }
+// }
 
-// Méthode pour initialiser le système et envoyer une requête
-int Cmodelisateur::init()
-{
-    if (initSys() == 0 && envoiRequeteSocket("i") == 0)
-    {
-        return 1;
-    }
-    return 0;
-}
+// // Méthode pour réaliser une campagne de photos
+// int Cmodelisateur::camp(int nbImage, const char *idPiece, const char *date)
+// {
 
-// Méthode pour réaliser une campagne de photos
-int Cmodelisateur::camp(int nbImage, const char *idPiece, const char *date)
-{
-    std::string dossier = "www/" + std::string(idPiece) + "_" + std::string(date);
-    system(("mkdir -p " + dossier).c_str());
+//     std::string dossier = "www/" + idPiece + "_" + date;
+//     system(("mkdir " + dossier).c_str());
+//     std::string requete = "INSERT INTO Campagne__de_photo (date, Chemin_d_acces, id_Piece) VALUES ('" + std::string(date) + "','" + dossier + "','" + std::string(iPiece) + "')";
 
-    std::string requete = "INSERT INTO Campagne__de_photo (date, Chemin_d_acces, id_Piece) VALUES ('" + std::string(date) + "','" + dossier + "','" + std::string(idPiece) + "')";
-    if (ecrireDB(requete.c_str()) != 0)
-    {
-        return 1;
-    }
+//     // Appel de la méthode ecrireDB avec la chaîne de requête
+//     this->ecrireDB(requete.c_str());
 
-    if (init() != 1)
-    {
-        return 2;
-    }
+//     if (init())
+//     {
+//         int nbdeplacement = int(floor(sqrt(nbimg)));
+//         // Calcul du nombre de déplacements en prenant la racine carrée du nombre d'images.
 
-    int nbdeplacement = static_cast<int>(std::floor(std::sqrt(nbImage)));
-    int intervalleRasp = static_cast<int>(std::floor(nbPasMaxRasp / nbdeplacement));
-    int intervalleEsp = static_cast<int>(std::floor(nbPasMaxEsp / nbdeplacement));
-    int cpt = 0;
-    int posEsp;
+//         int intervalleRasp = int(floor(nbPasMaxRasp / nbdeplacement));
+//         // Calcul de l'intervalle entre les déplacements pour Raspberry Pi en divisant nbPasMaxRasp par nbdeplacement.
 
-    for (int i = 0; i < nbPasMaxRasp; i += intervalleRasp)
-    {
-        posEsp = 0;
+//         int intervalleEsp = int(floor(nbPasMaxEsp / nbdeplacement));
+//         // Calcul de l'intervalle entre les déplacements pour ESP (un autre dispositif) en divisant nbPasMaxEsp par nbdeplacement.
 
-        deplacerMot(i, 1);
+//         int cpt = 0;
+//         // Compteur pour le nombre de photos prises.
 
-        for (int j = 0; j < (nbPasMaxEsp - intervalleEsp); j += intervalleEsp)
-        {
-            std::string requete = "p." + std::to_string(j) + ".1";
-            if (envoiRequeteSocket(requete.c_str()) == 0)
-            {
-                posEsp = j;
+//         int posEsp;
+//         // Variable pour garder la position actuelle de l'ESP.
 
-                if (prendrePhoto(dossier + "/" + std::to_string(cpt)) == 0)
-                {
-                    cpt++;
-                }
-                else
-                {
-                    return 3;
-                }
-            }
-            else
-            {
-                return 2;
-            }
-        }
+//         for (int i = 0; i < (nbpasmax); i += intervalleRasp)
+//         // Boucle pour déplacer le dispositif Raspberry Pi à des intervalles réguliers.
+//         {
+//             posEsp = 0;
+//             // Réinitialiser la position de l'ESP.
 
-        std::string requeteRetour = "p." + std::to_string(posEsp) + ".0";
-        if (envoiRequeteSocket(requeteRetour.c_str()) != 0)
-        {
-            return 4;
-        }
-    }
+//             if (this->deplacerMot(i, 1);)
+//             // Vérifie si le déplacement du dispositif Raspberry Pi est réussi.
+//             {
+//                 for (int j = 0; j < (nbpasmax - intervalle); j += intervalleEsp)
+//                 // Boucle pour déplacer l'ESP à des intervalles réguliers.
+//                 {
+//                     if (this->envoiRequeteSocket(("p." + std::to_string(j) + ".1").c_str());)
+//                     // Envoie une requête pour déplacer l'ESP.
+//                     {
+//                         posEsp = j;
+//                         // Met à jour la position actuelle de l'ESP.
 
-    return 0;
-}
+//                         if (this->prendrePhoto((dossier + "/" + cpt).c_str()))
+//                         // Prend une photo et l'enregistre dans le dossier spécifié avec un compteur.
+//                         {
+//                             cpt++;
+//                             // Incrémente le compteur de photos.
+//                         }
+//                         else
+//                         {
+//                             return 3;
+//                             // Retourne 3 si la prise de photo échoue.
+//                         }
+//                     }
+//                     else
+//                     {
+//                         return 2;
+//                         // Retourne 2 si l'envoi de la requête de déplacement de l'ESP échoue.
+//                     }
+//                 }
+//             }
+//             else
+//             {
+//                 return 1;
+//                 // Retourne 1 si le déplacement du dispositif Raspberry Pi échoue.
+//             }
+
+//             // Ajouter retour ESP à sa position initiale
+//             if (!(this->envoiRequeteSocket(("p." + std::to_string(posEsp) + ".0").c_str());))
+//             {
+//                 return 4;
+//                 // Retourne 4 si l'ESP ne peut pas être retourné à sa position initiale.
+//             }
+//         }
+//     }
+// }
+// else
+// {
+//     return 0;
+// }
+// }
