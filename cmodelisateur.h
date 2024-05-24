f3d92e473e6fbd47ddc7f3f3aed929e633d7ccd8
Wellan21 <tristan.marcoux21@gmail.com>
2024-05-24 15:01:48 +0200

    test complilation constructeur

--- cmodelisateur.h
+++ cmodelisateur.h
@@ -1,34 +1,32 @@
 //cmodelisateur.h 
 // Marcoux Tristan Avril 2024 
+#ifndef CMODELISATEUR_H
+#define CMODELISATEUR_H
 
-#ifndef Cmodelisateur 
-#define Cmodelisateur
-
-#include "clientSocket.h" 
+#include "clientSocket.h"
 #include <iostream>
 #include <sqlite3.h>
 
-class Cmodelisateur{
-    private: 
+class Cmodelisateur {
+private: 
     clientSocket client; 
-    int  pinCapteur ; 
-    int pinSteep ; 
-    int pinDirection ;
-    const char*  pathDB; 
+    int pinCapteur; 
+    int pinSteep; 
+    int pinDirection;
+    const char* pathDB; 
     int positionMot;
-    int nbPasMaxRasp ;
-    int nbPasMaxEsp ;
+    int nbPasMaxRasp;
+    int nbPasMaxEsp;
     int ecrireDB(const char* requete);
     void deplacerMot(int nbpas, int dir); 
     int envoiRequeteSocket(const char* requete); 
-    int initSys() ;
-    int prendrePhoto(std::string  * nomphoto);
-    public : 
-    int init() ;
+    int initSys(); 
+    int prendrePhoto(std::string* nomphoto);
+    
+public: 
+    int init(); 
     int camp(int nbImage, const char* idPiece, const char* date); 
-    Cmodelisateur(int pinCapteur, int pinSteep, int pinDir, const char* pathDB ,int nbPasMaxRasp ,int nbPasMaxEsp, const char ip, int port);
-    
-    
-} ; 
+    Cmodelisateur(int pinCapteur, int pinSteep, int pinDir, const char* pathDB, int nbPasMaxRasp, int nbPasMaxEsp, const char* ip, int port);
+};
 
-#endif  
+#endif // CMODELISATEUR_H
