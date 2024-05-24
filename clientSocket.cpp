59d9a8c826b8118174707e5a485a43e3f1f0779c
Wellan21 <tristan.marcoux21@gmail.com>
2024-05-24 13:39:43 +0200

    correction de bug

--- clientSocket.cpp
+++ clientSocket.cpp
@@ -1,59 +1,53 @@
-#include "clientSocket.h" 
+#include "clientSocket.h"
+#include <arpa/inet.h> // for inet_addr
+#include <unistd.h> // for close, write, read
+#include <cstring> // for strcpy
+#include <iostream> // for perror
 
- clientSocket(char* ipSrv, int portsrv) {
-        this->ip = ipSrv;
-        this->port = portsrv;
-    }
-clientSocket(){
-      this->ip ="1.1.1.1";
-        this->port = 80;
-    }
+clientSocket::clientSocket(const char* ipSrv, int portSrv) {
+    this->ip = std::string(ipSrv);
+    this->port = portSrv;
+}
 
-    bool envoyerMessage( char* msg) {
-        /* Déclaration des variables */
-        int sockfd_client;
-        int lg_client;
-        struct sockaddr_in adresse;
-        int resultat;
-        char ch = 'X';
+clientSocket::clientSocket() {
+    this->ip = "1.1.1.1";
+    this->port = 80;
+}
 
-        /* Création socket pour le client */
-        sockfd_client = socket(AF_INET, SOCK_STREAM, 0);
-        if (sockfd_client == -1) {
-            perror("Erreur lors de la création du socket");
-            return false;
-        }
+bool clientSocket::envoyerMessage(const char* msg) {
+    int sockfd_client;
+    struct sockaddr_in adresse;
+    int resultat;
+    char ch = 'X';
 
-        /* Nommage du socket avec les valeurs du constructeur */
-        adresse.sin_family = AF_INET;
-        adresse.sin_addr.s_addr = inet_addr(this->ip);
-        adresse.sin_port = htons(this->port);
-
-        lg_client = sizeof(adresse);
-
-        /* Connexion du socket à celui du serveur */
-        resultat = connect(sockfd_client, (struct sockaddr*)&adresse, lg_client);
-        if (resultat == -1) {
-            perror("Erreur lors de la connexion au serveur");
-            close(sockfd_client);
-            return 0;
-        }
-
-        /* Ecriture via sockfd_client */
-        write(sockfd_client, &ch, 1);
-
-        /* Lecture via sockfd_client */
-        read(sockfd_client, &ch, 1);
-
-       if (ch=='1'){
-        return true;
-       }
-       else [
+    sockfd_client = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd_client == -1) {
+        perror("Erreur lors de la création du socket");
         return false;
-       ]
-
-        /* Fermeture connexion côté client */
-        close(sockfd_client);
-
-        return true;
     }
+
+    adresse.sin_family = AF_INET;
+    adresse.sin_addr.s_addr = inet_addr(this->ip.c_str());
+    adresse.sin_port = htons(this->port);
+
+    int lg_client = sizeof(adresse);
+
+    resultat = connect(sockfd_client, (struct sockaddr*)&adresse, lg_client);
+    if (resultat == -1) {
+        perror("Erreur lors de la connexion au serveur");
+        close(sockfd_client);
+        return false;
+    }
+
+    write(sockfd_client, &ch, 1);
+
+    read(sockfd_client, &ch, 1);
+
+    if (ch == '1') {
+        close(sockfd_client);
+        return true;
+    } else {
+        close(sockfd_client);
+        return false;
+    }
+}
